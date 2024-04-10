ac3d5f13f77e2e7339279ef166490c2fd88b79e7
Esteban Sepulveda <tebansd@icloud.com>
2024-04-09 17:20:45 -0700

    set up basic barebones signup form from hover.dev framework

---
+++ frontend/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This should cover most React project structures
+    "./public/index.html", // If you're using any Tailwind classes directly in your HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,12 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import React from "react";
+import { SlideInAuth } from "./Components/SignUp"; 
+import './App.css';
 
-const socket = io('http://localhost:5001'); // Connect to the backend
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
-
-  useEffect(() => {
-    socket.on('chat message', (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
-    });
-  }, []);
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    if (input) {
-      socket.emit('chat message', input); // Send message to the backend
-      setInput(''); // Clear input after sending
-    }
-  };
 
   return (
-    <div>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
-      <form onSubmit={sendMessage}>
-        <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <button type="submit">Send</button>
-      </form>
-    </div>
+    <SlideInAuth />
   );
 }
 
