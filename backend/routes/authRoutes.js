42c7907d89b2f6f2fc61a4297e3b11b480f1e2cb
Esteban Sepulveda <tebansd@icloud.com>
2024-04-15 16:39:45 -0700

    fixed issue where password was being hashed twice, resulting in passwords never being matched.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,41 +7,45 @@ const router = express.Router();
 
 // Register route
 router.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: "Username is already taken." });
+    try {
+        const { username, password } = req.body;
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username is already taken." });
+        }
+
+        const user = new User({ username, password });
+        await user.save();
+
+        res.status(201).json({ message: "User registered successfully." });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ username, password: hashedPassword });
-    await user.save();
-
-    res.status(201).json({ message: "User registered successfully." });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
+
 
 // Login route
 router.post("/login", async (req, res) => {
-  try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(400).send("Invalid credentials.");
+    if (!user) {
+        console.log("User not found");
+        return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials." });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+        expiresIn: "1d",
     });
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+    res.json({ token: token, message: "Login successful!" });
 });
+
+
 
 module.exports = router;
