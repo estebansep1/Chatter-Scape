8beca5c47a7f7049f5e7d380e1fff1988c7120d7
Esteban Sepulveda <tebansd@icloud.com>
2024-04-15 23:17:01 -0700

    added protected route to ensure user is authenticated before accessing dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { SlideInAuth } from "./Components/SignUp";
 import Dashboard from "./Components/Dashboard";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import './App.css';
 
 function App() {
@@ -9,11 +10,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<SlideInAuth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
