a36c649cafef14d605e198e2b997498a4770ca57
Esteban Sepulveda <tebansd@icloud.com>
2024-04-24 17:07:04 -0700

    changed wording

--- frontend/src/Components/SettingsPage.jsx
+++ frontend/src/Components/SettingsPage.jsx
@@ -86,7 +86,7 @@ export default function SettingsPage() {
         <div className="space-y-12">
           <div className="text-center">
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Profile
+              Account Settings
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               This information will be displayed publicly so be careful what you
